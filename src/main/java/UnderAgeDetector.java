c3d8b69487d035ca20efb9845013d74b1aaff116
Klaudia <khturowska@gmail.com>
2018-10-14 14:16:49 +0200

    added another programs

--- src/main/java/UnderAgeDetector.java
+++ src/main/java/UnderAgeDetector.java
@@ -1,2 +1,25 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+
+/*
+6. Napisz program który sprawdzi czy osoba jest pełnoletnia.
+    Program powinien wczytać z konsoli datę urodzenia w formacie dd.MM.yyyy i powiedzieć czy osoba jest pełnoletnia czy nie.
+    Jak zamienić String na Date?
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+Date date = format.parse("2010-10-10");
+ */
 public class UnderAgeDetector {
+    public static void main(String[] args) {
+        System.out.println("Please state your date of birth in format: dd.MM.yyyy");
+        Scanner bt = new Scanner(System.in);
+        String dateOfbirth = bt.nextLine();
+
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date dateOfbirth = format.parse(dateOfbirth);
+
+        int year = dateOfbirth.getYear();
+    }
+
 }
